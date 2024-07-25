468895bd02e8bbd919ac8909e595a831aa12a890
Pedro Barboza <peh123barboza@gmail.com>
2024-07-25 13:43:13 -0300

    TASK03

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categorias/categorias.module';
+import { Categoria } from './categorias/entities/categorias.entity';
+
+@Module({
+  imports: [    
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '.753159Tata.',
+    database: 'db_minerva',
+    entities: [Categoria],
+    synchronize: true,
+    logging: true,
+}),
+CategoriaModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
