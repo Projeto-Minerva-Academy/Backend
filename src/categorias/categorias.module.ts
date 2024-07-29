47e3836fb10a576a4c7716d70b695ddf95aebf3b
julia <julialameiras@gmail.com>
2024-07-29 11:42:07 -0300

    TASK 04

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categoria } from './entities/categorias.entity';
+import { CategoriaService } from './services/categorias.service';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([Categoria])],
   controllers: [],
-  providers: [],
+  providers: [CategoriaService],
   exports: [TypeOrmModule],
 })
 export class CategoriaModule {}
