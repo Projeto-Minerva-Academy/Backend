29eee884ac68fc2419a7084bccf00da670e20465
GabrielBuchud <gabrielbuchudestudos@gmail.com>
2024-07-30 11:05:05 -0300

    Adicionado a module de produtos e a service

---
+++ src/produtos/produto.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Produto } from "./entities/produto.entity";
+import { ProdutoService } from "./services/produto.service";
+import { ProdutoController } from "./controller/produto.controller";
+import { CategoriaModule } from "../categorias/categorias.module";
+import { CategoriaService } from "../categorias/services/categorias.service";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Produto]), CategoriaModule],
+    providers: [ProdutoService, CategoriaService],
+    controllers: [ProdutoController],
+    exports: [TypeOrmModule]
+})
+export class ProdutoModule { }
