f2b5ca5cc8a298d4ef2331bec92b8700a9752759
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-02 12:45:46 -0300

    feat(auth): add auth module and changes imports

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -1,28 +1,22 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { AuthService } from '../service/auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Strategy } from 'passport-local';
-
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'usuario',
+      passwordField: 'senha',
+    });
+  }
 
-    constructor(private authService: AuthService) {
-        super({
-            usernameField: 'usuario',
-            passwordField: 'senha',
-        });
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
     }
-
-    async validate(username: string, password: string): Promise<any> {
-
-        const user = await this.authService.validateUser(username, password);
-        if (!user) {
-
-            throw new UnauthorizedException();
-
-        }
-        return user;
-    }
-
+    return user;
+  }
 }
