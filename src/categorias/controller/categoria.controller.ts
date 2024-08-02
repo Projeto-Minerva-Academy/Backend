528b38ffd9513060b2c82631ecfc602e4d0b5bdd
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-02 16:17:47 -0300

    chore(categoria): change controller jwt  guard

--- src/categorias/controller/categoria.controller.ts
+++ src/categorias/controller/categoria.controller.ts
@@ -15,7 +15,6 @@ import { CategoriaService } from '../services/categoria.service';
 import { Categoria } from '../entities/categoria.entity';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 
-@UseGuards(JwtAuthGuard)
 @Controller('/categorias')
 export class CategoriaController {
   constructor(private readonly categoriaService: CategoriaService) {}
@@ -26,30 +25,35 @@ export class CategoriaController {
     return this.categoriaService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/:id')
   @HttpCode(HttpStatus.OK) // Http Status 200
   findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
     return this.categoriaService.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/tipo/:tipo')
   @HttpCode(HttpStatus.OK) // Http Status 200
   findByTipo(@Param('tipo') tipo: string): Promise<Categoria[]> {
     return this.categoriaService.findByTipo(tipo);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   @HttpCode(HttpStatus.CREATED) // Http Status 201
   create(@Body() categoria: Categoria): Promise<Categoria> {
     return this.categoriaService.create(categoria);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put()
   @HttpCode(HttpStatus.OK) // Http Status 200
   update(@Body() categoria: Categoria): Promise<Categoria> {
     return this.categoriaService.update(categoria);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT) // Http Status 204
   delete(@Param('id', ParseIntPipe) id: number) {
